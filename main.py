b390cebecadd15a90c38a93bbd34ecaa48d93930
jonas de brito balero <72454186+jonasbb28@users.noreply.github.com>
2023-01-06 11:52:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
 # our main file.
 
-''' # Reconhecedor de voz do google
-import speech_recognition as sr
-
-# Criando um reconhecedor
-r = sr.Recognizer()
-
-# Abrindo o microfone para captura
-with sr.Microphone() as source:
-    while True:
-        audio = r.listen(source) # Define microfone como fonte de áudio
-        
-        if (r.recognize_google(audio, show_all=True) == []):
-            print("Não disse nada.")
-        else:
-            print(r.recognize_google(audio, language='pt', show_all=True)['alternative'][0]['transcript'])
-'''
-
 #!/usr/bin/env python3
 
 # prerequisites: as described in https://alphacephei.com/vosk/install and also python module `sounddevice` (simply run command `pip install sounddevice`)
